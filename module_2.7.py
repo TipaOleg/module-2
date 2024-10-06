2c0da866f7aa2e1183c721384e008c0b4259e2f4
TipaOleg <iroh2021@gmail.com>
2024-10-06 17:51:48 +0300

    rework for defs

--- module_2.7.py
+++ module_2.7.py
@@ -1,28 +1,36 @@
-n = int(input())
-finale_str = ''
-x = 1
-simple_list = []
+def is_valid_input(n):
+    return 3 <= n <= 20
 
 
-if 3 <= n <= 20:
-    for i in range(n):
-        if n % (i + 1) == 0 and i + 1 != 1 and i + 1 != 2:
-            simple_list.append(i + 1)
+def get_non_trivial_divisors(n):
+    divisors = []
+    for i in range(3, n + 1):
+        if n % i == 0:
+            divisors.append(i)
+    return divisors
 
 
+def generate_pairs(simple_list, n):
+    finale_str = ''
+    x = 1
     for i in range(n // 2):
         for num in simple_list:
             y = num - x
             if y > 1 and y != x:
                 finale_str += str(x) + str(y)
         x += 1
+    return finale_str
 
 
-    print(finale_str)
+def main():
+    n = int(input("Enter a number between 3 and 20: "))
+
+    if is_valid_input(n):
+        simple_list = get_non_trivial_divisors(n)
+        finale_str = generate_pairs(simple_list, n)
+        print(finale_str)
+    else:
+        print('wrong')
 
 
-else:
-    print('wrong')
-
-
-
+print(main())
